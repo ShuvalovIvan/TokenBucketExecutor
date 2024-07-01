98afe2b0321d94432fba9a6dd79d08494fba670f
Ivan S <vanyashuvalov@gmail.com>
2024-06-30 20:57:51 -0400

    Added Tests

--- src/executor.h
+++ src/executor.h
@@ -1,5 +1,6 @@
 #include <string>
 #include <functional>
+#pragma once
 
 
 namespace tbe {
@@ -10,13 +11,25 @@ struct TaskProperties {
 
 using Task = std::function<void()>;
 
-enum class ExecutorError : int16_t {};
+enum class ExecutorError : int16_t {
+  None = 0,
+  Error = -1
+};
 
 class Executor {
 public:
   virtual ~Executor() {}
 
   virtual ExecutorError exec(Task&& task, TaskProperties&& properties) = 0; 
+};
+
+
+class InlineExecutor : public Executor {
+public:
+  ExecutorError exec(Task&& task, TaskProperties&& properties) override {
+    task();
+    return ExecutorError::None;
+  }
 };
 
 }  // namespace tbe
--- tests/unit/test_main.cpp
+++ tests/unit/test_main.cpp
@@ -1,7 +1,85 @@
 #include <gtest/gtest.h>
 
 #include "executor.h"
+#include "thread_pool_executor.h"
 
-TEST(ExecutorTest, Dummy) {
+namespace tbe {
+
+
+class InlineExecutorTest : public ::testing::Test {
+protected:
+    InlineExecutor executor;
+};
+
+
+TEST_F(InlineExecutorTest, ExecutesTask) {
+    bool taskExecuted = false;
+    Task task = [&taskExecuted]() {
+      taskExecuted = true;
+    };
+    TaskProperties properties = {"test_user"};
+
+    executor.exec(std::move(task), std::move(properties));
+
+    EXPECT_TRUE(taskExecuted);
 }
 
+
+TEST_F(InlineExecutorTest, ReturnsNoError) {
+    int value = 0;
+    Task task = [&value]() {value = 1;};
+    TaskProperties properties = {"test_user"};
+
+    ExecutorError error = executor.exec(std::move(task), std::move(properties));
+
+    EXPECT_EQ(error, ExecutorError::None);
+    ASSERT_EQ(value, 1);
+}
+
+
+class ThreadPoolExecutorTest : public ::testing::Test {
+protected:
+    ThreadPoolExecutor executor{6}; 
+};
+
+// Test to ensure the task is executed
+TEST_F(ThreadPoolExecutorTest, ExecutesTask) {
+    std::atomic<bool> taskExecuted{false};
+    Task task = [&taskExecuted]() {
+        taskExecuted = true;
+    };
+    TaskProperties properties = {"test_user"};
+
+    executor.exec(std::move(task), std::move(properties));
+
+    for (int i = 0; i < 100; ++i) {
+        if (taskExecuted.load()) {
+            break;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    EXPECT_TRUE(taskExecuted);
+}
+
+
+TEST_F(ThreadPoolExecutorTest, ReturnsNoError) {
+    Task task = []() {};
+    TaskProperties properties = {"test_user"};
+
+    ExecutorError error = executor.exec(std::move(task), std::move(properties));
+
+    EXPECT_EQ(error, ExecutorError::None);
+}
+
+
+} 
+
+
+
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
